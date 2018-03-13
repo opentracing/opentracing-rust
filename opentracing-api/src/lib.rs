528570c706ceacc4b2d5308893fed15003421bd3
Stefano Pogliani <stefano-pogliani@users.noreply.github.com>
2018-03-13 12:06:12 +0000

    Add SpanContext trait. (#10)
    This changeset adds the SpanContext trait with support for
    baggage item iteration.
    
    Completes #6

--- opentracing-api/src/lib.rs
+++ opentracing-api/src/lib.rs
@@ -1,7 +1,9 @@
 #![doc(html_root_url = "https://docs.rs/opentracing-api/0.1.0")]
 
+mod context;
 mod tag;
 mod field;
 
+pub use context::SpanContext;
 pub use tag::{Tags, ParseTagsError};
 pub use field::{Fields, ParseFieldsError};
